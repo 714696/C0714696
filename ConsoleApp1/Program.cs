6b57ce31371f45488e4f9cd0a0bb67048cb8a2b2
visualstudio <visualstudio@230-PC12>
2019-03-28 16:18:58 -0400

    march 28

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace C0714696
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Peter Rules IT universe!");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("Download Complete");
         }
     }
 }
